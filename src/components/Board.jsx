cea9eaa96452c50a453af6f65e30b49af9a989b8
tipo159 <tipo159@gmail.com>
2022-04-27 05:48:50 +0900

    Step 3-4: Completing the Game - Declaring a Winner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -8,6 +8,9 @@ const Board = () => {
 
     const handleClick = (i) => {
         const new_squares = squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         new_squares[i] = xIsNext ? 'X' : 'O';
         setSquares(new_squares);
         setXIsNext(!xIsNext);
@@ -17,7 +20,13 @@ const Board = () => {
         return <Square value={squares[i]} onClick={() => handleClick(i)} />;
     }
 
-    const status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
 
     return (
         <div>
@@ -39,6 +48,26 @@ const Board = () => {
             </div>
         </div>
     );
+}
+
+const calculateWinner = (squares) => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 export default Board;
