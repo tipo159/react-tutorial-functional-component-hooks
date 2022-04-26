1dffb50218db3982acd37f16a7e696d2206a3dd7
tipo159 <tipo159@gmail.com>
2022-04-27 07:19:34 +0900

    Step 4-3: Adding Time Travel -

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,10 +4,11 @@ import Board from './Board'
 
 const Game = () => {
     const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+    const [stepNumber, setStepNumber] = useState(0);
     const [xIsNext, setXIsNext] = useState(true);
 
     const handleClick = (i) => {
-        const _history = history.slice();
+        const _history = history.slice(0, stepNumber + 1);
         const current = _history[_history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -15,11 +16,17 @@ const Game = () => {
         }
         squares[i] = xIsNext ? 'X' : 'O';
         setHistory(history.concat([{ squares: squares }]));
+        setStepNumber(history.length);
         setXIsNext(!xIsNext);
     }
 
-    const _history = history.slice();
-    const current = _history[_history.length - 1];
+    const jumpTo = (step) => {
+        setStepNumber(step);
+        setXIsNext((step % 2) === 0);
+    }
+
+    const _history = history;
+    const current = _history[stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -28,7 +35,7 @@ const Game = () => {
             'Go to game start';
         return (
             <li>
-                <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
             </li>
         );
     });
